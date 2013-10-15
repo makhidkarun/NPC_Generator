bf3f6bf97fdccbfcfc407c8454d7a5c2101cbb42
Leam Hall <leamhall@gmail.com>
2013-10-15 19:53:51 -0400

    Added autoload fiel. Still does nto work

---
+++ autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+function __autoload($class)
+{
+    $file = $class . '.php';
+    if (file_exists($file)) {
+        echo "How about that $file!";
+        require_once $file;
+    } else {
+        echo "$file does not exist.";
+    }
+}
+
