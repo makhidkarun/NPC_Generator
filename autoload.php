5ddfe6665a5408aee5056b74de7db24b171327a7
Leam Hall <leamhall@gmail.com>
2013-10-19 04:43:59 -0400

    Simple autoload script.

--- autoload.php
+++ autoload.php
@@ -2,12 +2,14 @@
 
 function __autoload($class)
 {
-    $file = $class . '.php';
+    $file = './' . $class . '.php';
     if (file_exists($file)) {
-        echo "How about that $file!";
+    //    echo "How about that $file!";
         require_once $file;
     } else {
-        echo "$file does not exist.";
+    //    echo "$file does not exist.";
+        throw new Exception("Unable to load $file.");
     }
 }
+
 
