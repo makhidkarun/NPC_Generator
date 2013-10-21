85e4f76e9430a7372b4399e380a60b3bacb53c46
Leam Hall <leamhall@gmail.com>
2013-10-21 07:22:22 -0400

    Added find() syntax to selection.

--- view_NCO.php
+++ view_NCO.php
@@ -4,16 +4,15 @@ $m = new MongoClient();
 $db = $m->traveller;
 $collection = $db->npcs;
 
-$cursor = $collection->find();
+$cursor = $collection->find(array('Rank'=>'1SG'));
 foreach ($cursor as $doc) {
     $line1 = '';
     $line1 .= $doc['Name'] . " ";
     $line1 .= $doc['Rank'] . " ";
     $line1 .= $doc['UPP'] . " ";
-   
     $line1 .= "\n";
+    echo $line1;
 }
 
-echo $line1;
  
 
