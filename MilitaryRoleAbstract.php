90a10895c142b84c0c0e4a80b45c75606471965a
Leam Hall <leamhall@gmail.com>
2013-11-02 09:02:47 -0400

    MilitaryRoleAbstract PSR2 compliant.

--- MilitaryRoleAbstract.php
+++ MilitaryRoleAbstract.php
@@ -1,17 +1,34 @@
 <?php
 
-// RoleAbstract.php
-// From MAXCHUCK on #phpmentoring
+/**
+ * MilitaryRoleAbstract.php
+ * 
+ * To be implemented in various ways.
+ *
+ * PHP Version 5
+ * 
+ * License: This is open source code of some license I don't yet know.
+ * 
+ * @category    Games
+ * @package     Makhidkarun
+ * @author      Leam Hall <leamhall@gmail.com>
+ * @copyright   2012-2013 Leam Hall
+ * @license     license 1 <http://LICENSEURL.example.com>
+ * @version     GIT: 0.2
+ * @link        github.com/makhidkarun/NPC_Generator/blob/master/FILE.php
+ * @see         huh?
+ * @since       Original copyright date 
+ * @assists     #phpmentoring MANCHUCK
+*/
 
 namespace NPC_Generator;
  
-abstract class MilitaryRoleAbstract {
+abstract class MilitaryRoleAbstract
+{
 
     protected $min_age;
     protected $max_age;
     protected $min_rank;
     protected $max_rank;
     protected $rank_group;
-
 }
-
